188ad64831f1558ca37d1b9516658804db1951bb
Justin Chai <justinbchai@gmail.com>
2025-06-29 16:51:24 -0400

    added playGame() function. Plays 5 rounds of rock paper scissors, and prints the final scores.

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 function getComputerChoice() {
     let random = Math.random() * 3;
-    console.log(random);
     if (random <= 1) {
-        return 'rock';
+        return 'Rock';
     } else if (random <= 2) {
-        return 'paper';
+        return 'Paper';
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
 
 function getHumanChoice() {
     let input = prompt('Type \'rock\', \'paper\', or \'scissors\'.');
     input = input.toLowerCase();
+    input = input.charAt(0).toUpperCase() + input.slice(1);
     return input;
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-    computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
 
-    if(humanChoice === computerChoice) {
-        // tie
-        console.log(`Tie! You both picked ${humanChoice}.`)
-    } else if (humanChoice === 'Rock' && computerChoice === 'Paper' || humanChoice === 'Paper' && computerChoice === 'Scissors' || humanChoice === 'Scissors' && computerChoice === 'Rock'){
-        // human lost
-        computerScore++;
-        console.log(`You lost! ${computerChoice} beats ${humanChoice}.`);
-    } else if (computerChoice === 'Rock' && humanChoice === 'Paper' || computerChoice === 'Paper' && humanChoice === 'Scissors' || computerChoice === 'Scissors' && humanChoice === 'Rock'){
-        // human won
-        humanScore++;
-        console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
-    } else {
-        console.log('Oops, something went wrong!!')
+        if (humanChoice === computerChoice) {
+            // tie
+            console.log(`Tie! You both picked ${humanChoice}.`)
+        } else if (humanChoice === 'Rock' && computerChoice === 'Paper' || humanChoice === 'Paper' && computerChoice === 'Scissors' || humanChoice === 'Scissors' && computerChoice === 'Rock') {
+            // human lost
+            computerScore++;
+            console.log(`You lost! ${computerChoice} beats ${humanChoice}.`);
+        } else if (computerChoice === 'Rock' && humanChoice === 'Paper' || computerChoice === 'Paper' && humanChoice === 'Scissors' || computerChoice === 'Scissors' && humanChoice === 'Rock') {
+            // human won
+            humanScore++;
+            console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+        } else {
+            console.log('Oops, something went wrong!!')
+        }
     }
+
+    const NUMBER_OF_ROUNDS = 5;
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < NUMBER_OF_ROUNDS; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    console.log(`Human score: ${humanScore}`)
+    console.log(`Computer score: ${computerScore}`)
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
