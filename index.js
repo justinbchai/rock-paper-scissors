63aa977803b6a9b0dccbdde57746f7e97b5e6160
Justin Chai <justinbchai@gmail.com>
2025-06-29 16:36:54 -0400

    added function playRound(humanChoice, computerChoice). prints win/loss/tie message depending on arguments

--- index.js
+++ index.js
@@ -15,3 +15,27 @@ function getHumanChoice() {
     input = input.toLowerCase();
     return input;
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+
+    if(humanChoice === computerChoice) {
+        // tie
+        console.log(`Tie! You both picked ${humanChoice}.`)
+    } else if (humanChoice === 'Rock' && computerChoice === 'Paper' || humanChoice === 'Paper' && computerChoice === 'Scissors' || humanChoice === 'Scissors' && computerChoice === 'Rock'){
+        // human lost
+        computerScore++;
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}.`);
+    } else if (computerChoice === 'Rock' && humanChoice === 'Paper' || computerChoice === 'Paper' && humanChoice === 'Scissors' || computerChoice === 'Scissors' && humanChoice === 'Rock'){
+        // human won
+        humanScore++;
+        console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+    } else {
+        console.log('Oops, something went wrong!!')
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
